77afa43aaf0c5c050473ef34890d15be8233d714
Quantumwubs <munchkin8896@gmail.com>
2018-05-23 14:44:00 +1000

    backup

--- bot.js
+++ bot.js
@@ -1,3 +1,26 @@
 
 const        Discord = require('discord.js');
 const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('bot ready!');
+});
+
+client.on("message", function (message) {
+  var channel = message.channel;
+  console.log(message.content);
+  if(message.content.indexOf("!") == 0) {
+    var command = message.content.substr(1);
+    console.log(command);
+    /*
+      Send message: channel.send(MESSAGE);
+      Get Command Sent: command
+    */
+
+    if (command == "help") {
+      channel.send("I cannot help you!");
+    }
+  }
+});
+
+client.login(String("NDQ4NzA0MDA5ODM1MTE4NjAy.DeZ_sg.sBbpsiN0hhoO7Ew2Z9A_PPj3-KM"));
