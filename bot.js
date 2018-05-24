e71fe37bab152a58ec606ed8a6c83a17fbbb6fb0
Quantumwubs <munchkin8896@gmail.com>
2018-05-24 13:16:20 +1000

    things

--- bot.js
+++ bot.js
@@ -2,8 +2,12 @@
 const        Discord = require('discord.js');
 const client = new Discord.Client();
 
+function ranBetween(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+
 client.on('ready', () => {
-    console.log('bot ready!');
+    console.log('bot ready');
 });
 
 client.on("message", function (message) {
@@ -19,16 +23,28 @@ client.on("message", function (message) {
 
     if (command == "help") {
       channel.send(
-"```markdown\nList Of Help Commands:\n"+"
- 1. !help\n"+"
-```");
-    } else if(command == "!BlazeBurnBlade") {
-      channel.send("The best person in this godamn place!")
+"```markdown\n# List Of Help Commands:\n"+
+"1. !help: "+ "Opens a list of helpful features\n"+
+"2. !arena <bet or fight> \n"+
+    "bet (blue or yellow): Bets on the blue or yellow team\n"+
+    "fight: allows you to play the arena minigame"
+"```");
 
+    }
+    else if(command == "arena bet blue") {
+      var blu = ranBetween(0, 1);
+      if(blu == 0){channel.send("The Blue team wins!")} else {channel.send("The Yellow team wins")}
+    }
+    else if(command == "arena bet yellow"){
+      var yel = ranBetween(0, 1);
+      if(yel == 0){channel.send("The Blue team wins!")} else {channel.send("The Yellow team wins")}
+    }
+    else if(command == "arena fight"){
+      var race = ranBetween(0, 8);
     } else {
-      channel.send("Not a command!");
+      channel.send("Not a command :()");
     }
   }
 });
 
-client.login(String("NDQ4NzA0MDA5ODM1MTE4NjAy.DeZ_sg.sBbpsiN0hhoO7Ew2Z9A_PPj3-KM"));
+client.login(String("NDQ4NzA0MDA5ODM1MTE4NjAy.Dee6yA.M9cEeaSWDX1TvW_XG7SbPOvzXpk"));
